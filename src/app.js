00a4d993e557a57cced795a16c2d4b25d03eca0b
Rich Greenhill <rgreenhill@thinkful.com>
2020-06-04 07:51:14 -0700

    add error handler; add example env

--- src/app.js
+++ src/app.js
@@ -18,4 +18,16 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello, world!' });
 });
 
+app.use(function errorHandler(error, req, res, next) {  // eslint-disable-line no-unused-vars
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { message: 'Internal server error occurred.' };
+  } else {
+    console.log(error);
+    response = { message: error.message };
+  }
+
+  res.status(500).json(response);
+});
+
 module.exports = app;
